4c26775d702840c2217392bb27f66f70aa84e912
Ba-Han <96304699+Ba-Han@users.noreply.github.com>
2023-05-23 11:34:34 +0630

    Change Redemption Setting.

--- CRM-App/src/app/modules/admin/setting/redemption setting/redemption.resolvers.ts
+++ CRM-App/src/app/modules/admin/setting/redemption setting/redemption.resolvers.ts
@@ -19,6 +19,39 @@ export class RedemptionsResolver implements Resolve<any>
 @Injectable({
     providedIn: 'root'
 })
+export class RedemptionsDetailResolver implements Resolve<any>
+{
+    constructor(
+        private _redemptionService: RedemptionService,
+        private _router: Router
+    ) {
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Redemption> {
+        return this._redemptionService.getRedemptionSettingById(Number(route.paramMap.get('id')))
+            .pipe(
+                // Error here means the requested product is not available
+                catchError((error) => {
+
+                    // Log the error
+                    console.error(error);
+
+                    // Get the parent url
+                    const parentUrl = state.url.split('/').slice(0, -1).join('/');
+
+                    // Navigate to there
+                    this._router.navigateByUrl(parentUrl);
+
+                    // Throw an error
+                    return throwError(error);
+                })
+            );
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
 export class MemberTierResolver implements Resolve<any>
 {
     constructor(private _redemptionService: RedemptionService) {
