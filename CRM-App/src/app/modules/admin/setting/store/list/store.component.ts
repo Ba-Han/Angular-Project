3439ab5da33ea68cb06f4d8a62f3f3473c6fc2c6
Ba-Han <96304699+Ba-Han@users.noreply.github.com>
2023-09-22 15:34:20 +0630

    fixed report issue.

--- CRM-App/src/app/modules/admin/setting/store/list/store.component.ts
+++ CRM-App/src/app/modules/admin/setting/store/list/store.component.ts
@@ -19,18 +19,18 @@ import { UserService } from 'app/core/user/user.service';
         /* language=SCSS */
         `
             .store-grid {
-                grid-template-columns: 200px 150px 150px 150px;
+                grid-template-columns: 200px 150px 150px 150px 150px;
 
                 @screen sm {
-                    grid-template-columns: 200px 150px 150px 150px;
+                    grid-template-columns: 200px 150px 150px 150px 150px;
                 }
 
                 @screen md {
-                    grid-template-columns: 200px 150px 150px 150px;
+                    grid-template-columns: 200px 150px 150px 150px 150px;
                 }
 
                 @screen lg {
-                    grid-template-columns: 200px 150px 150px 150px;
+                    grid-template-columns: 200px 150px 150px 150px 150px;
                 }
             }
 
